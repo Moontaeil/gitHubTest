d62a86071f988d7600dcf60392fa4f28c375fa11
Moon <xodlf39@gmail.com>
2024-08-06 12:10:58 +0900

    테스트 중ㅇ

--- Project_Board_EX/src/main/java/kr/co/sol/Config/SevletAppContext.java
+++
@@ -1,5 +0,0 @@
-package kr.co.sol.Config;
-
-public class SevletAppContext {
-
-}
---
+++ Project_Board_EX/src/main/java/test/TestaMain.java
@@ -0,0 +1,8 @@
+package test;
+
+public class TestaMain {
+	public static void main(String[] args) {
+		System.out.println("테스트 중입니다.!!!");
+		System.out.println("00000000000000");
+	}
+}
